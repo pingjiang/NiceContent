c04ac5c776aaaf07a1b678846ef62e821428aaf2
pingjiang <pingjiang1989@gmail.com>
2014-08-08 16:05:08 +0800

    优化选项页面样式

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -7,16 +7,8 @@ var remarked_options = {
     h2_setext:  true,     // underline h2 headers
     h_atx_suf:  false,    // header suffixes (###)
     gfm_code:   false,    // gfm code blocks (```)
-    li_bullet:  "*",      // list item bullet style
-    hr_char:    "-",      // hr style
-    indnt_str:  "    ",   // indentation string
-    bold_char:  "*",      // char used for strong
-    emph_char:  "_",      // char used for em
-    gfm_del:    true,     // ~~strikeout~~ for <del>strikeout</del>
     gfm_tbls:   true,     // markdown-extra tables
     tbl_edges:  false,    // show side edges on tables
-    hash_lnks:  false,    // anchors w/hash hrefs as links
-    br_only:    false,    // avoid using "  " as line break indicator
 };
 
 jQuery(document).ready(function($) {
